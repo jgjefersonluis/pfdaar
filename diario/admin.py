cd23dd7e5223ec52744a95be503c252464902909
Jeferson Luis <jgjefersonluis@gmail.com>
2021-06-20 04:24:02 -0300

    app diario funcionalidades completo

--- diario/admin.py
+++ diario/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from .models import Diario
+
+
+@admin.register(Diario)
+class DiarioAdmin(admin.ModelAdmin):
+    list_display = ('data', 'hora', 'local', 'profissional', 'acao', 'sujeitos', 'avaliacao')
